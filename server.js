fe8afbdc2a1d0ff7dd5e13b6576ad0a76e48fa83
cocol <129044860+kimkangwoo@users.noreply.github.com>
2024-05-26 18:36:02 +0900

    --

--- server.js
+++ server.js
@@ -1,56 +1,27 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
-const path = require('path');
-const session = require('express-session');
-const nunjucks = require('nunjucks');
-const dotenv = require('dotenv');
-const passport = require('passport');
-const db = require('./Database');
 const mysql = require('mysql');
+const path = require('path');
+const router = require('./routes')
+const nunjucks = require('nunjucks');
 
-dotenv.config();
-const port = process.env.PORT;
 
+
+const index = require('./routes/index');
+
+app.set('port', 3000);
+app.set("veiw engine", 'html');
+
+nunjucks.configure('views', {
+    express : app,
+    watch: true
+});
+
+// route 설정 //////////////////////////////////////////////////
 const app = express();
 
-const connection = mysql.createConnection(db)
-connection.connect();
+app.use('/', router)
 
-app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'html');
-nunjucks.configure('views', {
-    express: app,
-    watch : true,
-});
-
-// cookie-parser 미들웨어 사용
-app.use(cookieParser(dotenv.COOKIE_NAME));
-
-app.get('/', function(req, res){
-    console.log("main 페이지 요청...");
-
-  // 쿠키는 req.cookies 객체를 통해 읽을 수 있습니다.
-  console.log('요청된 쿠키:', req.cookies);
-  res.cookie('greeting', '741545465');
-  // 클라이언트에 응답을 보낼 수도 있습니다.
-  res.send('쿠키를 확인했습니다.');
-});
-
-app.get('/design', function(req, res){
-    res.render('main');
-});
-app.get('/db', function(req, res){
-    connection.query("SELECT * FROM book;", function(err, row, fields){
-        if (err) throw err;
-        else{
-            console.log("데이터베이스 연결에 성공했습니다.");
-            res.send(row);
-        }
-    })
-});
-
-
-app.listen(port, function(){
-    console.log(`server is running : http://localhost:${port}`);
+app.listen(app.get('port'), ()=>{
+    console.log('현재 서버가 실행중입니다.');
+    console.log(`주소 : http://localhost:${app.get('port')}`);
 });
