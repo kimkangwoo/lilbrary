9033ccecce6a56917621fb63f6a0c1d4cf0bdc0d
201902001 <kmjkmj0020@gmail.com>
2024-06-10 14:45:08 +0900

    insert session

--- routes/intro.js
+++ routes/intro.js
@@ -2,18 +2,33 @@
 // 소개 페이지 기능 구현
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const router = express.Router();
 router.use(cookieParser());
+
+router.use(session({
+    secret : 'secret',
+    reseave: false,
+    saveUnitialized: true
+}));
 
 router.get('/', (req, res, next) => {
     
     if(req.cookies.user){
         user =  JSON.parse(req.cookies.user);
+        req.session.user = user.users_name;
     }else{
         user ="";
     }
-    console.log(user);
+
+    if (req.session.user) {
+        console.log(`로그인된 사용자: ${req.session.user}`);
+    } else {
+        console.log('로그인되지 않았습니다.');
+    }
+
+
     res.render('intro', {
         title : "libmng : 메인",
         user : user,
